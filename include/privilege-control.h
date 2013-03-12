cb043eb135d2f28a2e0f25e8f00ffbce3fe5122d
Jinkun Jang <jinkun.jang@samsung.com>
2013-03-13 01:51:32 +0900

    Tizen 2.1 base

---
+++ include/privilege-control.h
@@ -0,0 +1,180 @@
+/*
+ * libprivilege control
+ *
+ * Copyright (c) 2000 - 2012 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Contact: Kidong Kim <kd0228.kim@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _PRIVILEGE_CONTROL_H_
+#define _PRIVILEGE_CONTROL_H_
+
+/* Macros for converting preprocessor token to string */
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#ifndef API
+#define API __attribute__((visibility("default")))
+#endif // API
+
+/* error codes */
+#define	PC_OPERATION_SUCCESS		((int)0)
+#define PC_ERR_FILE_OPERATION		-1
+#define PC_ERR_MEM_OPERATION		-2
+#define PC_ERR_NOT_PERMITTED		-3
+#define PC_ERR_INVALID_PARAM		-4
+#define PC_ERR_INVALID_OPERATION	-5
+
+/* APIs - used by applications */
+int control_privilege(void) __attribute__((deprecated));
+
+int set_privilege(const char* pkg_name) __attribute__((deprecated));
+
+/**
+ * Set DAC and SMACK privileges for application.
+ * This function is meant to be call by the application launcher just before
+ * it launches an application. It will setup DAC and SMACK privileges based
+ * on app type and accesses.
+ * It must be called with root privileges, which will be dropped in the function.
+ *
+ * @param name package name
+ * @param type application type (currently distinguished types: "wgt" and other)
+ * @param path file system path to the binary
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int set_app_privilege(const char* name, const char* type, const char* path);
+
+/* APIs for WRT */
+
+/**
+* Set DAC and SMACK privileges for web application.
+* This is a specialized version of set_app_privilege() to be called by WRT
+* when it is being launched from the console instead of AUL.
+*
+* @param widget_id widget identificator
+* @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+*/
+int wrt_set_privilege(const char* widget_id);
+
+/**
+ * Reset all SMACK permissions for a widget.
+ * This function should be called when previously granted permissions
+ * for a widget are no longer needed (e.g. after its termination or
+ * deinstallation).
+ * It must be called by privileged user.
+ *
+ * @param widget_id widget identifier from WRT
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int wrt_permissions_reset(const char* widget_id);
+
+/**
+ * Grant SMACK permissions required to use selected devcaps.
+ * This function should be called during preparation for widget run
+ * (after wrt_permissions_reset()) and whenever widget is supposed to
+ * gain any new devcap permissions.
+ * It must be called by privileged user.
+ *
+ * @param widget_id widget identifier from WRT
+ * @param devcap_list array of devcap names, last element must be NULL
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int wrt_permissions_add(const char* widget_id, const char** devcap_list);
+
+/**
+ * Recursively set SMACK labels for a widget source directory.
+ * This function should be called once during widget installation, after
+ * widget's source is unpacked in it's destination directory.
+ * Results will be persistent on the file system.
+ * It must be called by privileged user.
+ *
+ * @param widget_id widget identifier from WRT
+ * @param path parent directory path with widget's source
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int wrt_set_src_dir(const char* widget_id, const char *path);
+
+/**
+ * Recursively set SMACK labels for a widget data directory.
+ * This function should be called once during widget installation, after
+ * widget's initial data is unpacked in it's destination directory.
+ * Results will be persistent on the file system.
+ * It must be called by privileged user.
+ *
+ * @param widget_id widget identifier from WRT
+ * @param path parent directory path with widget's data
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int wrt_set_data_dir(const char* widget_id, const char *path);
+
+/**
+ * For a UNIX socket endpoint determine if the other side is a widget
+ * and return its widget id.
+ *
+ * @param sockfd socket file descriptor
+ * @return id of the connecting widget on success, NULL on failure.
+ * Caller is responsible for freeing the return widget id.
+ */
+char* wrt_widget_id_from_socket(int sockfd);
+
+/**
+ * Grant SMACK permissions based on permissions list.
+ * It is intended to be called during app installation.
+ * It will construct SMACK rules based on permissions list, grant them
+ * and store it in a file, so they will be automatically granted on
+ * system boot.
+ * It must be called by privileged user.
+ *
+ * @param app_id application identifier
+ * @param perm_list array of permission names, last element must be NULL
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int app_add_permissions(const char* app_id, const char** perm_list);
+
+/**
+ * Revoke SMACK permissions from an application.
+ * This function should be called during app deinstallation.
+ * It will revoke all SMACK rules previously granted by  app_add_permissions().
+ * It will also remove a rules file from disk.
+ * It must be called by privileged user.
+ *
+ * @param app_id application identifier
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int app_revoke_permissions(const char* app_id);
+
+/**
+ * Recursively set SMACK labels for an application directory.
+ * This function should be called once during app installation.
+ * Results will be persistent on the file system.
+ * It must be called by privileged user.
+ *
+ * @param app_id application identifier
+ * @param path directory path
+ * @return PC_OPERATION_SUCCESS on success, PC_ERR_* on error
+ */
+int app_label_dir(const char* app_id, const char* path);
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // _PRIVILEGE_CONTROL_H_
---
+++ packaging/libprivilege-control.spec
@@ -0,0 +1,109 @@
+%define udev_libdir /usr/lib/udev
+
+Name:       libprivilege-control
+Summary:    Library to control privilege of application
+Version:    0.0.19
+Release:    1
+Group:      System/Security
+License:    Apache 2.0
+Source0:    %{name}-%{version}.tar.gz
+Source1:    %{name}-conf.manifest
+Source2:    smack-default-labeling.service
+BuildRequires: cmake
+BuildRequires: pkgconfig(libsmack)
+
+%description
+development package of library to control privilege of in-house application
+
+%package devel
+Summary:    Control privilege of application (devel)
+Group:      Development/Libraries
+Requires:   %{name} = %{version}-%{release}
+
+%description devel
+Library to control privilege of application (devel)
+
+%package conf
+Summary:    Control privilege of application files
+Group:      Development/Libraries
+Requires:   %{name} = %{version}-%{release}
+Requires:   /usr/bin/chsmack
+
+%description conf
+Library to control privilege of application files
+
+
+%prep
+%setup -q
+
+%build
+cmake . -DCMAKE_INSTALL_PREFIX=%{_prefix} -DCMAKE_BUILD_TYPE=%{?build_type:%build_type}
+
+make %{?jobs:-j%jobs}
+
+%install
+rm -rf %{buildroot}
+mkdir -p %{buildroot}/usr/share/license
+cp LICENSE.APLv2.0 %{buildroot}/usr/share/license/%{name}
+%make_install
+
+mkdir -p %{buildroot}/etc
+mv %{buildroot}/opt/etc/passwd %{buildroot}/etc/passwd
+mv %{buildroot}/opt/etc/group %{buildroot}/etc/group
+
+cp -a %{SOURCE1} %{buildroot}%{_datadir}/
+install -D -d %{buildroot}/etc/rc.d/rc3.d/
+install -D -d %{buildroot}/etc/rc.d/rc4.d/
+ln -sf ../init.d/smack_default_labeling %{buildroot}/etc/rc.d/rc3.d/S45smack_default_labeling
+ln -sf ../init.d/smack_default_labeling %{buildroot}/etc/rc.d/rc4.d/S45smack_default_labeling
+ln -sf ../init.d/smack_rules %{buildroot}/etc/rc.d/rc3.d/S02smack_rules
+ln -sf ../init.d/smack_rules %{buildroot}/etc/rc.d/rc4.d/S02smack_rules
+
+mkdir -p %{buildroot}%{_libdir}/systemd/system/basic.target.wants
+install -m 644 %{SOURCE2} %{buildroot}%{_libdir}/systemd/system/
+ln -s ../smack-default-labeling.service %{buildroot}%{_libdir}/systemd/system/basic.target.wants/
+
+%post
+if [ ! -e "/home/app" ]
+then
+        mkdir -p /home/app
+fi
+
+if [ ! -e "/home/developer" ]
+then
+        mkdir -p /home/developer
+fi
+
+chown 5000:5000 /home/app
+chmod 755 /home/app
+chown 5100:5100 /home/developer
+chmod 755 /home/developer
+
+if [ ! -e "/usr/share/privilege-control" ]
+then
+        mkdir -p /usr/share/privilege-control/
+fi
+
+ln -s %{udev_libdir}/rules.d/95-permissions-slp.rules /lib/udev/rules.d/95-permissions-slp.rules
+
+%files
+%{_libdir}/*.so.*
+%{_bindir}/slp-su
+%{_datarootdir}/privilege-control/*
+%{udev_libdir}/rules.d/*
+%attr(755,root,root) %{udev_libdir}/uname_env
+%{_datadir}/license/%{name}
+
+%files conf
+/etc/group
+/etc/passwd
+/opt/etc/smack/*
+%attr(755,root,root) /etc/rc.d/*
+%{_libdir}/systemd/system/smack-default-labeling.service
+%{_libdir}/systemd/system/basic.target.wants/smack-default-labeling.service
+%manifest %{_datadir}/%{name}-conf.manifest
+
+%files devel
+%{_includedir}/*.h
+%{_libdir}/*.so
+%{_libdir}/pkgconfig/*.pc
