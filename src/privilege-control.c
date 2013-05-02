618655f8840efd978b073ce9239a16e1d061d14b
Marcin Niesluchowski <m.niesluchow@samsung.com>
2013-05-02 14:18:32 +0200

    Bug in app_install() fixed
    [Issue#]        SSDWSSP-223
    [Bug/Feature]   Fix bugs that make libprivilege-control test fail
    [Cause]         N/A
    [Solution]      N/A
    [Verification]  N/A
    
    Change-Id: I88712168c64c8d35e7700124ff9da4ffefa32493

--- src/privilege-control.c
+++ src/privilege-control.c
@@ -1286,6 +1286,10 @@ API int app_install(const char* app_id)
 	if (ret != PC_OPERATION_SUCCESS)
 		return ret;
 
+        if (access(smack_path, F_OK) == 0){
+                C_LOGE("file exists");
+                return PC_ERR_FILE_OPERATION;
+        }
 	fd = open(smack_path, O_RDWR|O_CREAT, 0644);
 	if (fd == -1) {
 		C_LOGE("file open failed: %s", strerror(errno));
