61864e718cb60d7f684743c8d8bd9f47e1d32733
Pawel Polawski <p.polawski@partner.samsung.com>
2013-07-12 17:06:20 +0200

    Loading all smack-app rules during boot
    [Issue#]        SSDWSSP-407
    [Bug/Feature]   All smack-app rules should be loaded during boot
    [Cause]         Rules must be loaded without libprivilege database
    [Solution]      Rules are loaded from path
    [Verification]  All rules from smack-app dir should
                    be loaded after reboot automatic
    
    Change-Id: I7229a3209630b26392c57cd4a21471ec354ddc5b

--- rule_loader/rule_loader.c
+++ rule_loader/rule_loader.c
@@ -4,80 +4,80 @@
 #include <access-db.h>
 #include <privilege-control.h>
 #include <common.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+
+#define LOG_PATH    "/root/rules_loader_log.txt"
+#define EARLY_RULES_DIR   "/opt/etc/smack-app/accesses.d"
 
 int main(int argc, char * argv[])
 {
     FILE * log_file = NULL;
-    char ** installed_app_list = NULL;
-    int app_list_len = 0;
-    int i = 0;
     int ret = 0;
+    //for searching files in directory
+    struct dirent *file = NULL;
+    DIR *dir = NULL;
 
-    log_file = fopen("/root/rules_loader_log.txt", "w");
+    log_file = fopen(LOG_PATH, "w");
     if (log_file == NULL)
         goto error;
-    fprintf(log_file, "Loading rules for all installed apps:\n");
+    fprintf(log_file, "Loading early rules...\n");
 
-    //getting app list from database
-    ret = get_all_apps_ids(&installed_app_list, &app_list_len);
-    if (ret != PC_OPERATION_SUCCESS) {
-        fprintf(log_file, "Unable to load applications list!\n);");
-        ret = -1;
+    //opening directory
+    dir = opendir(EARLY_RULES_DIR);
+    if (dir == NULL) {
+        fprintf(log_file, "Error in opendir(): %s\n", strerror(errno));
         goto error;
     }
 
-    //checking if database is not empty
-    if (app_list_len == 0) {
-        fprintf(log_file, "App database empty, no apps installed\n");
-        goto error;
-    }
-
-    //loading rules for each app
-    for (i = 0; i < app_list_len; i++) {
-        fprintf(log_file, "Loading rules for app_id: %s...\n", installed_app_list[i]);
-
+    //iterate trough files
+    while ((file = readdir(dir)) != NULL) {
+        //for each file load rules from it
+        fprintf(log_file, "Loading rulse from file: %s\n", file->d_name);
+        //checking if it is file
+        if (file->d_type != DT_REG) {
+            fprintf(log_file, "Skipping, not regular file\n");
+            continue;
+        }
         //checking if rules are loaded
-        ret = check_if_rules_were_loaded(installed_app_list[i]);
+        ret = check_if_rules_were_loaded(file->d_name);
         if (ret < 0) {
             fprintf(log_file,"Error in check_if_rules_were_loaded()\n");
             //we skipping to the next app_id
         } else if (ret == 0) {
             //if not load rules
-            ret = add_app_first_run_rules(installed_app_list[i]);
+            ret = add_app_first_run_rules(file->d_name);
             if (ret < 0)
-                fprintf(log_file,"Error in add_app_first_run_rules()\n");
+                fprintf(log_file,"Error in add_app_first_run_rules(): %d\n", ret);
             else
                 //mark rules as loaded
-                mark_rules_as_loaded(installed_app_list[i]);
+                mark_rules_as_loaded(file->d_name);
         } else {
             fprintf(log_file,"Rules already loaded\n");
         }
 
-        fprintf(log_file, "DONE\n");
     }
 
-   ret = add_app_first_run_rules("email-service");
-   if (ret < 0)
-       fprintf(log_file,"Error in add_app_first_run_rules()\n");
-   else
-       //mark rules as loaded
-       mark_rules_as_loaded("email-service"); 
+    ret = add_app_first_run_rules("email-service");
+    if (ret < 0)
+        fprintf(log_file,"Error in add_app_first_run_rules()\n");
+    else
+        //mark rules as loaded
+        mark_rules_as_loaded("email-service"); 
 
-   ret = add_app_first_run_rules("contacts-service");
-   if (ret < 0)
-       fprintf(log_file,"Error in add_app_first_run_rules()\n");
-   else
-       //mark rules as loaded
-       mark_rules_as_loaded("contacts-service"); 
+    ret = add_app_first_run_rules("contacts-service");
+    if (ret < 0)
+        fprintf(log_file,"Error in add_app_first_run_rules()\n");
+    else
+        //mark rules as loaded
+        mark_rules_as_loaded("contacts-service"); 
+
+    fprintf(log_file, "DONE\n");
 
 error:
-    //cleaning up
-    if (installed_app_list != NULL) {
-        for (i = 0; i < app_list_len; i++)
-            free(installed_app_list[i]);
-        free(installed_app_list);
-    }
-
+    if (dir != NULL)
+        closedir(dir);
     if (log_file)
         fclose(log_file);
     return ret;
