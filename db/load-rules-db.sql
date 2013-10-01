3988a6b179c9ac9edfb9ee52a69719a7f4dddd77
Jan Olszak <j.olszak@samsung.com>
2013-10-01 10:55:34 +0200

    Change in boot script.
    [Issue#]       SSDWSSP-183
    [Bug/Feature]  N/A
    [Cause]        N/A
    [Solution]     Used redirection to write rules to kernel.
    [Verification] Build, install, reboot,
                   run systemctl status smack-rules.service
    
    Change-Id: I7034823ba6f124a9793657078ada70fc175f53e6

--- db/load-rules-db.sql
+++ db/load-rules-db.sql
@@ -11,7 +11,7 @@ DELETE FROM app_permission WHERE is_volatile = 1;
 
 
 .output "/opt/etc/smack/boot-rules.smack"
-SELECT subject, object, access_to_str(access)
+SELECT subject, object, access_to_str(access), "-"
 FROM   all_smack_binary_rules
 WHERE  all_smack_binary_rules.is_volatile = 0;
 COMMIT TRANSACTION;
