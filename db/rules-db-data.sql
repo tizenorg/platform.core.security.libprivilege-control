8acd4c5b1db2a04226b3fd4193547fafca814bca
Sebastian Grabowski <s.grabowski@partner.samsung.com>
2013-11-07 13:16:18 +0100

    Move appsetting and antivirus privileges to smack-privilege-conf
    [Issue#]        SSDWSSP-607
    [Bug/Feature]   Move appsetting and antivirus privileges to
                    smack-privilege-conf repository
    [Cause]         Since wildcards implementation in smack rule templates
                    appsetting and antivirus privileges no longer needs to
                    be added with rules-db-data.sql script.
    [Solution]      Removed obsolete sql statements.
                    Dependency from smack-privilege-config:
                    Ife85ad691fe036d1dfb9cfdb489d9e24ecba1947
    [Verification]  Test content of permission,
                    permission_app_path_type_rule_view and
                    permission_app_path_type_rule_view tables/views in
                    .rules-db.db3 after libprivilege-control installation
                    and after smack-privilege-config:
                    Delete rules db:
                    rm /opt/dbspace/.rules-db.db3
                    Delete smack rules:
                    rm /usr/share/privilege-control/*.smack
                    Install libprivilege-control with this change:
                    rpm -Uvh --force --nodeps /tmp/rpm/libprivilege-control-*
                    Run the following sqlite3 commands:
                    sqlite3 -column /opt/dbspace/.rules-db.db3 "select * from permission;"
                    sqlite3 -column /opt/dbspace/.rules-db.db3 "select * from permission_permission_rule_view;"
                    sqlite3 -column /opt/dbspace/.rules-db.db3 "select * from permission_app_path_type_rule_view;"
                    After these commands there should be no
                    org.tizen.privilege.appsetting nor
                    org.tizen.privilege.antivirus entries.
    
                    Next, install smack-privilege-config (with
                    Ife85ad691fe036d1dfb9cfdb489d9e24ecba1947 changes):
                    rpm -Uvh --force --nodeps /tmp/rpm/smack-privilege-config-*
                    Run the following sqlite3 commands (the same like above):
                    sqlite3 -column /opt/dbspace/.rules-db.db3 "select * from permission;"
                    sqlite3 -column /opt/dbspace/.rules-db.db3 "select * from permission_permission_rule_view;"
                    sqlite3 -column /opt/dbspace/.rules-db.db3 "select * from permission_app_path_type_rule_view;"
                    After these commands proper org.tizen.privilege.appsetting and
                    org.tizen.privilege.antivirus entries should be in the
                    database.
    
    Change-Id: I329fb361ea57b6cc86650b00074439eedad1c0d3

--- db/rules-db-data.sql
+++ db/rules-db-data.sql
@@ -42,67 +42,6 @@ INSERT OR IGNORE INTO permission_app_path_type_rule_view(permission_name,
 						is_reverse) VALUES
 	("ALL_APPS", "ALL_APPS", "PUBLIC_PATH", "rx", 0);
 
-
--- SETTINGS ----------------------------------------------------------------------------------------
--- Permission name == TIZEN_PRIVILEGE_APPSETTING
--- SETTINGS_PATH
-INSERT OR IGNORE INTO permission_view(name, type_name) VALUES
-		("org.tizen.privilege.appsetting", "WRT"),
-		("org.tizen.privilege.appsetting", "OSP"),
-		("org.tizen.privilege.appsetting", "EFL");
-
-INSERT OR IGNORE INTO permission_app_path_type_rule_view(permission_name,
-						permission_type_name,
-						app_path_type_name,
-						access,
-						is_reverse) VALUES
-	("org.tizen.privilege.appsetting","WRT", "SETTINGS_PATH", "rwx", 0),
-	("org.tizen.privilege.appsetting","OSP", "SETTINGS_PATH", "rwx", 0),
-	("org.tizen.privilege.appsetting","EFL", "SETTINGS_PATH", "rwx", 0);
-
-INSERT OR IGNORE INTO permission_permission_rule_view(permission_name,
-						      permission_type_name,
-						      target_permission_name,
-						      target_permission_type_name,
-						      access,
-						      is_reverse) VALUES
-	("org.tizen.privilege.appsetting","WRT", "ALL_APPS", "ALL_APPS", "rx", 0),
-	("org.tizen.privilege.appsetting","OSP", "ALL_APPS", "ALL_APPS", "rx", 0),
-	("org.tizen.privilege.appsetting","EFL", "ALL_APPS", "ALL_APPS", "rx", 0);
-
-
--- ANTIVIRUS ---------------------------------------------------------------------------------------
--- Permission name == TIZEN_PRIVILEGE_ANTIVIRUS
-INSERT OR IGNORE INTO permission_view(name, type_name) VALUES
-		("org.tizen.privilege.antivirus", "WRT"),
-		("org.tizen.privilege.antivirus", "OSP"),
-		("org.tizen.privilege.antivirus", "EFL");
-
-INSERT OR IGNORE INTO permission_permission_rule_view(permission_name,
-						      permission_type_name,
-						      target_permission_name,
-						      target_permission_type_name,
-						      access,
-						      is_reverse) VALUES
-	("org.tizen.privilege.antivirus","WRT",	"ALL_APPS", "ALL_APPS", "rwx", 0),
-	("org.tizen.privilege.antivirus","OSP", "ALL_APPS", "ALL_APPS",	"rwx", 0),
-	("org.tizen.privilege.antivirus","EFL", "ALL_APPS", "ALL_APPS", "rwx", 0);
-
-INSERT OR IGNORE INTO permission_app_path_type_rule_view(permission_name,
-						permission_type_name,
-						app_path_type_name,
-						access,
-						is_reverse) VALUES
-	("org.tizen.privilege.antivirus","WRT", "GROUP_PATH",    "rwx", 0),
-	("org.tizen.privilege.antivirus","OSP", "GROUP_PATH",    "rwx", 0),
-	("org.tizen.privilege.antivirus","EFL", "GROUP_PATH",    "rwx", 0),
-	("org.tizen.privilege.antivirus","WRT", "SETTINGS_PATH", "rwx", 0),
-	("org.tizen.privilege.antivirus","OSP", "SETTINGS_PATH", "rwx", 0),
-	("org.tizen.privilege.antivirus","EFL", "SETTINGS_PATH", "rwx", 0),
-	("org.tizen.privilege.antivirus","WRT", "PUBLIC_PATH",   "rwx", 0),
-	("org.tizen.privilege.antivirus","OSP", "PUBLIC_PATH",   "rwx", 0),
-	("org.tizen.privilege.antivirus","EFL", "PUBLIC_PATH",   "rwx", 0);
-
 COMMIT TRANSACTION;
 
 VACUUM;
