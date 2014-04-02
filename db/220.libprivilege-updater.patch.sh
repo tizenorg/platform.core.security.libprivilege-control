c71dd3eadc22a229d688279fbb8c8cae252abe8f
Rafal Krypa <r.krypa@samsung.com>
2014-04-02 18:50:07 +0200

    Remove remaining parts of "additional rules" feature.
    Fix improper revert of commit "Adding additional rules" (c7914a7f28).
    Parts of that were removed in commit "Adapt to tizenorg" (479d21a75f),
    but not all.
    
    Change-Id: Ie093158c887aad56790334072f5bc47cda0c0aab

--- db/220.libprivilege-updater.patch.sh
+++ db/220.libprivilege-updater.patch.sh
@@ -3,4 +3,3 @@
 /usr/share/privilege-control/db/updater.sh
 
 /usr/bin/api_feature_loader --verbose --dir=/usr/share/privilege-control/
-/usr/bin/api_feature_loader --verbose --rules=/usr/share/privilege-control/ADDITIONAL_RULES.smack
