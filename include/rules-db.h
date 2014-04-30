c50e753eb4b11f98b720c5fb18647302e8b6ec43
Rafal Krypa <r.krypa@samsung.com>
2014-04-30 11:40:23 +0200

    Restore perm_app_has_permission API function.
    Function can be used to query if an app has been granted a permission.
    It will be used temporarily by initial implementation of Cynara.
    Later Cynara will use it's own policy storage, but this is needed to setup
    a prototype.
    
    Change-Id: Idb5ea4eb904849f8c3694f4841052d495071a98c

--- include/rules-db.h
+++ include/rules-db.h
@@ -196,5 +196,22 @@ int rdb_revoke_app_permissions(const char *const s_app_label_name);
  */
 int rdb_reset_app_permissions(const char *const s_app_label_name);
 
+/**
+ * Check if app has the privilege that is specified by the name.
+ *
+ * @ingroup RDB API functions
+ *
+ * @param  s_app_label_name       application's label name
+ * @param  s_permission_type_name permission's type name
+ * @param  s_permission_name      permission name
+ * @param  p_is_enabled           buffer for return value
+ * @return                        PC_OPERATION_SUCCESS on success,
+ *                                error code otherwise
+ */
+int rdb_app_has_permission(const char *const s_app_label_name,
+			   const char *const s_permission_type_name,
+			   const char *const s_permission_name,
+			   bool *const p_is_enabled);
+
 
 #endif /*_RULES_DB_H_*/
